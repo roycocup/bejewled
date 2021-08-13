17728cf96a16225fac4f2106597d7e222b665d88
Rodrigo Dias <rodrigo.pitta@gmail.com>
2021-08-13 22:54:35 +0100

    events

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -8,14 +8,15 @@ export(int) var border = 10
 var hOffset = 17
 var vOffset = 100
 
+var first_touch
+var second_touch
+
+
 var grid = []
 var grid_map = {}
-
 func _ready():
 	make_grid()
 	populate_grid()
-
-
 func make_grid():
 	for i in rows:
 		grid.append([])
@@ -31,4 +32,33 @@ func populate_grid():
 			var pos = Vector2(x, y)
 			piece.set_id(str(j.row)+"-"+str(j.col))
 			piece.set_position(pos)
+			piece.connect("piece_clicked", self, "_on_piece_clicked")
+			j['piece'] = piece
 			add_child(piece)
+
+func reset_touches():
+	first_touch = null
+	second_touch = null
+
+func _on_piece_clicked(id):
+	if first_touch == null:
+		first_touch = id
+	elif second_touch == null:
+		if first_touch != id:
+			second_touch = id
+			handle_two_touches()
+
+func handle_two_touches():
+	reset_touches()
+	swap()
+func swap():
+	print(grid)
+	# var tween = get_node("Tween")
+	# tween.interpolate_property($Node2D, "position",
+	# 		Vector2(), Vector2(100, 100), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	# tween.start()
+	pass
+
+	
+	
+
--- scripts/Piece.gd
+++ scripts/Piece.gd
@@ -4,17 +4,14 @@ const SIZE = 64
 const BORDER = 10
 var piece_space = SIZE + BORDER
 signal piece_clicked(id)
-var piece_id: String
-
+var id: String
 func _ready():
 	var colour = get_random_piece()
 	var image = load("res://assets/" + colour + ".png")
 	get_node("TextureRect").texture = image
-	
-func set_id(id):
-	piece_id = id
-	
-	
+func set_id(piece_id):
+	id = piece_id
+		
 func get_random_piece():
 	var pieces = ['yellow','blue', 'green', 'red']
 	randomize()
@@ -23,12 +20,10 @@ func get_random_piece():
 
 func _input(event):
 	if event.is_action_released("ui_select"):
-		var mousePos = get_global_mouse_position()
 		var origin = transform.get_origin()
-		var r = Rect2(origin.x, origin.y, SIZE, SIZE).has_point(event.position)
-		if r:
-			emit_signal("piece_clicked", piece_id)
-#			print (piece_id, event.position)
+		if Rect2(origin.x, origin.y, SIZE, SIZE).has_point(event.position):
+			emit_signal("piece_clicked", id)
+
 		
 		
 
